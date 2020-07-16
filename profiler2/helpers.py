76b8682ab21590f83079f2279b7bb2b39748c757
Josh Schmelzle <13954434+joshschmelzle@users.noreply.github.com>
2020-07-16 01:44:03 -0400

    remove epilog from help output

--- profiler2/helpers.py
+++ profiler2/helpers.py
@@ -179,7 +179,6 @@ def setup_parser() -> argparse:
             a Wi-Fi client analyzer for identifying supported 802.11 capabilities
             """
         ),
-        epilog="made with Python by Josh Schmelzle",
         fromfile_prefix_chars="2",
     )
     parser.add_argument(
